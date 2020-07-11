c7642bb805a978ec3025610d8cbb8576f78b764e
omrobbie <putu.roby@yahoo.com>
2020-07-11 18:01:34 +0800

    setup notification

--- ios-push-notification/ViewController.swift
+++ ios-push-notification/ViewController.swift
@@ -12,8 +12,34 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNotification()
+    }
+
+    private func setupNotification() {
+        UNUserNotificationCenter.current().delegate = self
+
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+                if granted {
+                    print("Mendapatkan izin dari pengguna untuk local notifications")
+                } else {
+                    print("Tidak mendapatkan izin dari pengguna untuk local notifications")
+                }
+        }
     }
 
     @IBAction func btnNotifTapped(_ sender: Any) {
+    }
+}
+
+extension ViewController: UNUserNotificationCenterDelegate {
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
+
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("UserInfo yang terkait dengan notifikasi == \(response.notification.request.content.userInfo)")
+        completionHandler()
     }
 }
